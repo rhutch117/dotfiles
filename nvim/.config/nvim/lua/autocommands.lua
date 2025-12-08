635ee4156f54d040138deb8e0dbff4501cc7c5ca
Ryan Hutchison <rwhutchison@live.com>
2025-12-08 08:16:14 -0600

    add yank highlighting and update dockls alias

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -23,3 +23,15 @@ vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'markdown' },
     command = 'setlocal wrap',
 })
+
+-- [[ Highlight Yanked Text ]]
+-- This autocommand triggers a highlight when text is yanked (copied)
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch', -- Use the IncSearch highlight group
+            timeout = 300, -- Highlight lasts for "X" ms
+        })
+    end,
+})
