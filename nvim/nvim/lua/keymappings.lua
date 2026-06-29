0400424a373f8ae8f98984d22da756523e3cff83
ryan <rwhutchison@live.com>
2026-06-29 12:46:47 -0500

    add a way to search for functions/methods in a file

--- nvim/nvim/lua/keymappings.lua
+++ nvim/nvim/lua/keymappings.lua
@@ -66,6 +66,12 @@ vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iag
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>sy', function()
+    builtin.lsp_document_symbols({ symbols = { 'function', 'method' } })
+end, { desc = '[y] Search symbols in current file' })
+
+
+
 -- Shortcut for searching your Neovim configuration files
 vim.keymap.set('n', '<leader>sn', function()
     builtin.find_files({ cwd = vim.fn.stdpath('config') })
